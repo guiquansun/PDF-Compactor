0acd0fe99949570640f48deac2e00b6a75076f87
Guiquan <gsun@usc.edu>
2020-04-29 22:24:04 -0700

    really final commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# PDF Compactor Project
+a web application that allows users to upload a pdf file and return a compacted version of the file based on the user’s specified DPI. 
+
+# Process Of the whole full_stack
+<img alt="frontend-backend-diagram.png" src="./image/frontend-backend-diagram.png" width="400" text-align="center">
+
+# Tools
+## Frontend
+ Bootstrap + HTML + JQuery + CSS
+## Backend  
+ Node.js + express + multer + Ghostscript
+## The pdf compression rools URL
+https://github.com/ourarash/shrinkpdf
+
+# Start The Project
+Firstly, install NodeJs (https://nodejs.org/en/download/) and Ghostscript (check this link carefully https://github.com/ourarash/shrinkpdf). 
+```bash
+git clone https://github.com/guiquansun/PDF-Compactor.git
+cd PDF-Compactor
+npm install
+```
+
+Running:
+```bash
+node app.js
+```
+Now open your browser to http://localhost:3000
+<img alt="first-page" src="./image/first-page.png" width="400">
+Upload a pdf file and the page is switched.
+<img alt="second-page" src="./image/second-page.png" width="400">
+CLick Process button to do the compression
+<img alt="third-page" src="./image/third-page.png" width="400">
+See your history files
+<img alt="fourth-page" src="./image/fourth-page.png" width="400">
+
+# Group members
+Linxin Mai linxinma@usc.edu
+Guiquan Sun  gsun@usc.edu
---
+++ app.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const path = require('path');
+const ejs = require('ejs');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const dir = __dirname;
+
+app.use(express.urlencoded({extended: false})); //analysis http request
+app.use(express.static(path.join(dir, 'public/'))); // default public
+app.use(express.static(path.join(dir, 'image/'))); // image file
+app.set('view engine', 'ejs');
+app.use('/static/compacted', express.static('compactedFiles'));
+app.use('/static/original', express.static('originalFiles'));
+
+let count = 0;
+
+function getOriginalFileName(filename) {
+    filename = filename.replace(/[- ]+/g, '_');
+    const index = filename.lastIndexOf('.pdf');
+    if (index == -1) {
+        return filename + '.pdf';
+    }
+    return filename;
+}
+
+function getCompactedFileName(filename) {
+    const index = filename.lastIndexOf('.pdf');
+    if (index != -1) {
+        return filename.substring(0, index) + '-' + 'compacted.pdf';
+    }
+    return filename + '-' + 'compacted.pdf';
+}
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(dir, 'originalFiles'));
+    },
+
+    filename: function(req, file, cb) {
+        console.log(file);
+        let filename = getOriginalFileName(file.originalname);
+        req.filename = filename;
+        cb(null, filename);
+    }
+})
+
+const fileFilter = function(req, file, cb) {
+    // if (!file) {
+    //     req.error = 'Error: File not exists';
+    //     console.log(req.error);
+    //     cb();
+
+    // }
+    console.log(file);
+    const type = file.mimetype;
+
+    if (type === 'application/pdf') {
+
+        cb(null, true);
+    }
+    // else {
+    //     req.error = 'Error: This is not a pdf file.';
+    //     console.log(req.error);
+    //     cb();
+    // }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter, limits: {fileSize: 30*1024*1000}}); // 上传文件的中间件
+
+app.get('/', function(req, res) {
+    res.render('index', {count: count});
+});
+
+app.post('/upload', upload.single('pdf'), function(req, res) {
+    if (req.error) {
+        res.render('error', {error: req.error});
+        return;
+    }
+
+    res.render('uploaded', {filename: req.filename});
+});
+
+app.post('/compact', function(req, res) {
+    const originalFile = req.body.filename;
+    let stats = fs.statSync(path.join(dir, "originalFiles/" + originalFile));
+    console.log(stats.size);
+    const compactedFile = getCompactedFileName(originalFile);
+
+    const resolution = req.body.resolution;
+
+    const quality = req.body.level;
+    var command = `./shrinkpdf.sh ./originalFiles/${originalFile} ./compactedFiles/${compactedFile}`;
+    if (quality == 300 || quality == 144 || quality == 60) {
+        command = `./shrinkpdf.sh ./originalFiles/${originalFile} ./compactedFiles/${compactedFile} ${quality}`;
+    }
+    else {
+        command = `./shrinkpdf.sh ./originalFiles/${originalFile} ./compactedFiles/${compactedFile} ${resolution}`;
+
+    }
+    console.log(command);
+    exec(command, (err, stdout, stderr) => {  //execute
+      if (err) {
+          console.log('Error: Compacting error');
+          res.render('error', {error: err});
+      } else {
+          console.log('Compacting successfully');
+
+          let statsCompress = fs.statSync(path.join(dir, "compactedFiles/" + compactedFile));
+          console.log(statsCompress.size);
+          res.render('compacted', {compactedFile: compactedFile, stats: Math.floor(stats.size / 1000), statsCompress: Math.floor(statsCompress.size / 1000)});
+
+      }
+
+    });
+    count++;
+})
+
+app.get('/history', async function(req, res) {
+    const files = await fs.promises.readdir('./originalFiles');
+    const PDFfiles = [];
+    for (const file of files) {
+        if (file.endsWith('.pdf')) {
+            PDFfiles.push([file, getCompactedFileName(file)]);
+        }
+    }
+    console.log(PDFfiles);
+    res.render('history', {pdfFiles: PDFfiles});
+})
+
+app.get('/delete', function(req, res) {
+    console.log(req.query.fileName);
+    const fileName = req.query.fileName;
+    fs.unlinkSync('originalFiles/' + fileName);
+    fs.unlinkSync('compactedFiles/' + getCompactedFileName(fileName));
+    res.json({status: 'success'});
+});
+
+app.listen(3000, function() {
+    console.log("Server Listenining on port 3000");
+});
